63750d8827e31c03edad0062d01c556fb090754f
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-15 10:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var a = parseInt(prompt("Enter first number"));
 var b = parseInt(prompt("Enter second number"));
 
-alert((a*2) + b);
+var result = ((a*2) + b);
+
+alert("The final result is: " + result);
